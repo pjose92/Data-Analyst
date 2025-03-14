d0a06c8fa4810a1b1afd1403d2fe61590b903eb7
Jose Perez Guerrero <pjose14.jp38@yahoo.com>
2025-03-13 23:24:17 -0500

    orginized files

---
+++ sql_queries/windowFuctions.sql
@@ -0,0 +1,27 @@
+-- Window Funtions
+
+SELECT gender, AVG(salary) as avg_salary
+FROM employee_demographics dem
+JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id
+GROUP BY gender;
+
+SELECT dem.first_name, dem.last_name, gender, 
+AVG(salary) OVER(PARTITION BY gender)
+FROM employee_demographics dem
+JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id;
+    
+SELECT dem.first_name, dem.last_name, gender, salary,
+SUM(salary) OVER(PARTITION BY gender ORDER BY dem.employee_id) AS Roling_Total
+FROM employee_demographics dem
+JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id;
+    
+SELECT dem.employee_id, dem.first_name, dem.last_name, gender, salary,
+ROW_NUMBER() OVER(PARTITION BY gender ORDER BY salary DESC) AS row_num,
+RANK() OVER(PARTITION BY gender ORDER BY salary DESC) AS rank_num,
+DENSE_RANK() OVER(PARTITION BY gender ORDER BY salary DESC) AS dense_rank_num
+FROM employee_demographics dem
+JOIN employee_salary sal
+	ON dem.employee_id = sal.employee_id;
